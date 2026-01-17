2a2d13a00f6f3588ebd877141ab36695ecf0dff3
Jacob Santos <jacobsantos@jacobsantos.com>
2026-01-16 20:18:59 -0600

    feat: remove `SetupHookApiUseCase` since the Service Provider should handle it
    Signed-off-by: Jacob Santos <jacobsantos@jacobsantos.com>

--- src/Contracts/UseCases/SetupHookApiUseCaseInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace SpeedySpec\WP\Hook\Domain\Contracts\UseCases;
-
-interface SetupHookApiUseCaseInterface {
-    public function setupHookApi(): void;
-}
